8097292ee0f8d83a6a6bf0cb302f9d1c057be512
Sambad <patdio40@hotmail.com>
2021-11-25 11:55:55 +0100

    modifications index.html

--- js/index.js
+++ js/index.js
@@ -31,16 +31,14 @@ const selectionnerVille = (event) =>{
  
      })
     .then(function(data) {
-      //console.log(data);
+      //récupération des données de ma ville
       maville.innerHTML = data.city.name;
         lat.innerHTML= data.city.coord.lat; 
         long.innerHTML= data.city.coord.lon; 
         pays.innerHTML=  " "+ data.city.country; 
         //on boucle sur la météo des 5 jours
-        // let dataList=data.list[4];
-
-        // console.log(dataList.weather);
-        for(let i = 4 ; i <= data.list.length;i--){
+        
+        for(let i = 0; i <= data.list[4].length; i++){
            console.log("data.list"+[i]);
            /* let div = document.createElements('div'); 
             div.document.s
